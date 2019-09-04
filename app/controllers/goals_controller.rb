3e0b1e62230c151ee6b2f50d92533b1b548569d1
Mary Banshee <mary.banshee@gmail.com>
2019-09-04 20:48:08 +0300

    Add step 3

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,4 +1,23 @@
 class GoalsController < ApplicationController
   def index
   end
+
+  def new
+    @goal = Goal.new
+  end
+
+  def create
+    @goal = Goal.new(goal_params)
+    if @goal.save
+      flash[:success] = 'Your goal has been successfully created'
+      redirect_to goals_path
+    else
+      flash[:error] = 'Please fill in all necessary fields.'
+      render 'new'
+    end
+  end
+
+  def goal_params
+    params.require(:goal).permit(:title, :description, :priority, :due_date, :complete)
+  end
 end
---
+++ db/migrate/20190904173421_create_goals.rb
@@ -0,0 +1,11 @@
+class CreateGoals < ActiveRecord::Migration[5.2]
+  create_table :goals do |t|
+    t.string :title, null: false
+    t.text :description, null: false
+    t.date :due_date, null: false
+    t.integer :priority, null: false
+    t.boolean :complete, null: false, default: false
+
+    t.timestamps
+  end
+end
